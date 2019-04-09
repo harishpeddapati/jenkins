3ff9e623e8dd97cffdbc6c51b4259e352c0201fb
harishpeddapati <harishpeddapati@gmail.com>
2019-04-09 13:11:52 +0530

    adding helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+class helloworld:
+	def __init__(self):
+		for i in range(10):
+			print(i,"Hello world")
+
+
+def main():
+	obj = helloworld()
+	
+
+if __name__ == "__main__":
+	main()
