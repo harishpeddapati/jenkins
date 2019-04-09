a6ee32584373005eddc4e6a8d4dc2f535965b7ee
harishpeddapati <harishpeddapati@gmail.com>
2019-04-09 13:58:46 +0530

    f4 added

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
+import time
+
 class helloworld:
 	def __init__(self):
 		for i in range(10):
 			print(i,"Hello world")
+                        time.sleep(5)
 
 
 def main():
